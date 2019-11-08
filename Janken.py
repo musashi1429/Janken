03c4ed2983d83584790a5d3891670fe0c6a4c0ba
kassama fally <fally.kassama@outlook.com>
2019-11-08 12:07:53 +0100

    add set

--- Janken.py
+++ Janken.py
@@ -10,6 +10,8 @@ print("bonjour",pseudo)
 pfc = ("Pierre", "Feuille", "Ciseaux")
 joueurscore = 0
 iascore = 0
+manche_joueur = 0
+manche_ia = 0
 
 # add while for the game.The game can upload score and pseudo in json File.
 while input("Jouez (y/n): ").lower() != "n":
@@ -18,19 +20,19 @@ while input("Jouez (y/n): ").lower() != "n":
     print("Choisissez un chiffre entre 0 et 2 sachant que Pierre>Ciseaux>Feuille>Pierre ")
     print("------------------------------------\n")
 
-    a = int(input("Choisissez un chiffre:\n0: Pierre\n1: Feuille\n2: Ciseaux\n-> "))
-    b = choice(range(3))
+    joueur = int(input("Choisissez un chiffre:\n0: Pierre\n1: Feuille\n2: Ciseaux\n-> "))
+    ia = choice(range(3))
 
-    print("\n{} VS {}".format(pfc[a], pfc[b]))
+    print("\n{} VS {}".format(pfc[joueur], pfc[ia]))
 # if choice is equal point is null.
-    if a == b:
+    if joueur == ia:
         print("EGALITE\n")
         info = {pseudo : "EGALITE"}
         fichier = open("pseudoscore.json","a")
         fichier.write(json.dumps(info))
         fichier.close()
 #  if choice of player win add one point in joueurscore.
-    elif (a>b and b+1==a) or (a<b and a+b==2):
+    elif (joueur>ia and ia+1==joueur) or (joueur<ia and joueur+ia==2):
         joueurscore += 1
         print("GAGNEZ\n")
         info = {pseudo : "GAGNEZ"}
@@ -49,16 +51,18 @@ while input("Jouez (y/n): ").lower() != "n":
 # if somebody have 3 points first he win and the point become null for all.
     if joueurscore == 3 :
         print("vous ete arrivé a 3 point manche gagnante !!")
+        manche_joueur +=1
         iascore = 0
         joueurscore = 0
 # if somebody have 3 points first he win and the point become null for all.
     if iascore == 3 :
         print("ia est arrivé a 3 point manche gagnante !!")
+        manche_ia += 1
         iascore = 0
         joueurscore = 0
-
-print("vous avez obtenu : ",joueurscore)
-print("et l'ia a obtenu : ",iascore)
+# print set win of players
+print("manche joueur gagné : ",manche_joueur)
+print("manche ia gagné : ",manche_ia)
 
 
 # def read():
