9b5094e7a1059cc172e15f253287f9ef749230ed
kassama fally <fally.kassama@outlook.com>
2019-11-08 11:58:07 +0100

    udpate and commentarie add

--- Janken.py
+++ Janken.py
@@ -1,14 +1,17 @@
+from random import choice
+import json
+
+# insert pseudo of player
 print("Pour acceder au jeu merci d'inserer votre pseudo ou nom")
 pseudo = input("qu'elle est votre nom?\n ->  ")
 
-
-
-from random import choice
-import json
+#print hi and pseudo and add variable of score and rock paper scisor.
 print("bonjour",pseudo)
 pfc = ("Pierre", "Feuille", "Ciseaux")
+joueurscore = 0
+iascore = 0
 
-
+# add while for the game.The game can upload score and pseudo in json File.
 while input("Jouez (y/n): ").lower() != "n":
 
     print("\n------------------------------------")
@@ -19,30 +22,49 @@ while input("Jouez (y/n): ").lower() != "n":
     b = choice(range(3))
 
     print("\n{} VS {}".format(pfc[a], pfc[b]))
+# if choice is equal point is null.
     if a == b:
-        print("ÉGALITÉ\n")
-        info = {pseudo : "ÉGALITÉ"}
+        print("EGALITE\n")
+        info = {pseudo : "EGALITE"}
         fichier = open("pseudoscore.json","a")
         fichier.write(json.dumps(info))
         fichier.close()
+#  if choice of player win add one point in joueurscore.
     elif (a>b and b+1==a) or (a<b and a+b==2):
+        joueurscore += 1
         print("GAGNEZ\n")
         info = {pseudo : "GAGNEZ"}
         fichier = open("pseudoscore.json","a")
         fichier.write(json.dumps(info))
         fichier.close()
+# if iascore choice win add one point in iascore
     else:
+        iascore += 1
         print("PERDU\n")
         info = {pseudo : "PERDU"}
         fichier = open("pseudoscore.json","a")
         fichier.write(json.dumps(info))
         fichier.close()
-else:
-    def read():
-        pseudoscore = []
-        with open("pseudoscore.json") as f:
-            data = json.load(f)
-            for entrie in data:
-                read.append(entrie["":""])
-            return read
-print(read)
+
+# if somebody have 3 points first he win and the point become null for all.
+    if joueurscore == 3 :
+        print("vous ete arrivé a 3 point manche gagnante !!")
+        iascore = 0
+        joueurscore = 0
+# if somebody have 3 points first he win and the point become null for all.
+    if iascore == 3 :
+        print("ia est arrivé a 3 point manche gagnante !!")
+        iascore = 0
+        joueurscore = 0
+
+print("vous avez obtenu : ",joueurscore)
+print("et l'ia a obtenu : ",iascore)
+
+
+# def read():
+#     pseudoscore = []
+#     with open("pseudoscore.json") as f:
+#         data = json.load(f)
+#         for entrie in data:
+#             read.append(entrie["":""])
+#         return read
